8b8c68359478ec874a32911b97d3bcfac27740ee
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-08-13 14:02:57 -0600

    firebase setup

--- firebase-9-dojo/src/index.js
+++ firebase-9-dojo/src/index.js
@@ -1,1 +1,12 @@
-console.log('uwu')
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCSQNYIkehaGompCMgLXXbPVk-VEvgxrKE",
+  authDomain: "netninja-fb9-dojo.firebaseapp.com",
+  projectId: "netninja-fb9-dojo",
+  storageBucket: "netninja-fb9-dojo.appspot.com",
+  messagingSenderId: "1027421951075",
+  appId: "1:1027421951075:web:aea8eeb20226666b045456"
+};
+
+initializeApp(firebaseConfig);
